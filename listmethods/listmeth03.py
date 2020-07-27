ecbca6c278bed5a057ba480b927ecf8fa762e47f
Greg Green <gcg145@gmail.com>
2020-07-27 17:02:37 -0400

    list methods and mix list exercises

---
+++ listmethods/listmeth03.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# list containing different snakes
+snakes = ["cobra", "python", "black snake"]
+
+# This will print snakes
+print(snakes)
+
+# This will extend the given value "viper" to the list snakes
+snakes.extend("viper")
+print(snakes)
+
+# This will append the given value "viper" to the list snakes
+snakes.append("viper")
+print(snakes)
+
+# This will remove the last item in the list snakes
+snakes.pop()
+print(snakes)
