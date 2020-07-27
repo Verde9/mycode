374574970a068ebb9544ee36b8eeddf6c4ec1a3c
Verde9 <49665466+Verde9@users.noreply.github.com>
2020-07-27 14:58:36 -0700

    Update listmeth04.py

--- listmethods/listmeth04.py
+++ listmethods/listmeth04.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
-col_list = ["blue", "Columbus"]
-
-col_list.append(1492)
-
+# Asks for to input name
 name = input("What is your name? ")
 
+# Creates a list in a variable "col_list"
+col_list = ["blue", "Columbus"]
+
+# Appends value "1492" to list "col_list"
+col_list.append(1492)
+
+# Prints the following string using the given list arguements
 print(f"{col_list[2]}, {col_list[1]} sailed the ocean {col_list[0]}. {name} fell off the boat. :(")
