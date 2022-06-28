d96c96f46da8ba5622367a92ce09e59dda731b4e
Greg Green <gcg145@gmail.com>
2022-06-28 19:42:34 +0000

    join and split

---
+++ challenge02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+
+        user_name = input("What is your name?:")
+
+        day_input = input("What is the day of the week?")
+
+        print("Hello, " + user_name + "! " + "Happy " + day_input + "!")
+
+main()        
