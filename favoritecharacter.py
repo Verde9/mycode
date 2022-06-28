55798694188bdc795448db179f607fdf3f109d8a
Greg Green <gcg145@gmail.com>
2022-06-28 22:05:57 +0000

    favorite character

---
+++ favoritecharacter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/eenv python3
+
+def main():
+
+    rock_lee= {"name": "Rock Lee", "age": "14", "clan": "Lee Clan", "specialty": "taijutsu", "top abilities": ["Eight Gates", "Leaf Whirlwind", "Drunken Fist"]}
+
+main()    
